94f9b2891cc52c1f396582168c5c49cfa8f81e3e
XsidX <developer.sid@outlook.com>
2022-11-14 13:38:21 +0300

    Write automated tests for models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context 'associations' do
+    it('has many categories') do
+      user = User.reflect_on_association(:categories)
+      association = user.macro
+      expect(association).to eq(:has_many)
+    end
+
+    it('has many transactions') do
+      user = User.reflect_on_association(:transactions)
+      association = user.macro
+      expect(association).to eq(:has_many)
+    end
+  end
+
+  context 'validations' do
+    it('is invalid without a name') do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+
+    it('is invalid with a name longer than 30 characters') do
+      user = User.new(name: 'a' * 31)
+      expect(user).to_not be_valid
+    end
+  end
+
+end
