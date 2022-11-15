81a416e8a235470d404540784b1d176fd88c0582
XsidX <developer.sid@outlook.com>
2022-11-15 16:44:31 +0300

    Add the create new category modal/form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,22 @@ class CategoriesController < ApplicationController
     @categories = current_user.categories
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      flash[:success] = "Category created!"
+    else
+      flash[:danger] = "Category not created!"
+    end
+    redirect_to categories_path
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
